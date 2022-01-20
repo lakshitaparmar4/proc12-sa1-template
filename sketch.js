c7654a94ed6e7a484513dbbce29f1544e0458cb5
lakshitaparmar4 <97336143+lakshitaparmar4@users.noreply.github.com>
2022-01-20 19:18:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,14 @@ var marks = [35,38,42,45,43,34,46,41,48,32];
 function setup() 
 {
   createCanvas(400,400);
+  for(var i = 0; i<marks.length;i++)
+  {
+  if(marks[i]>=45)
+  {
+  console.log(marks[i]);
+  }
+  }
+  
 
 }
 
